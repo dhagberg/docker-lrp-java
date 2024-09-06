71b862aeb4d870e893e64ec6faa99714ce5c33a1
D. J. Hagberg <dhagberg@millibits.com>
2024-09-06 16:32:10 +0000

    arg count check

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ CI_NAME=cga-ci:5000/$PLAIN_NAME
 docker build --pull -t $HUB_NAME .
 
 # Tag and push if given
-if [ "$1" = "push" ]; then
+if [ $# -ge 1 ]; then
+  if [ "$1" = "push" ]; then
     docker push $HUB_NAME
     docker tag  $HUB_NAME $CI_NAME
     docker push $CI_NAME
+  fi
 fi
